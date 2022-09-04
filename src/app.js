b6f2246abfeeb8d011c23f48d43c965a999dfd6a
Katemarkiza <111583089+Katemarkiza@users.noreply.github.com>
2022-09-04 22:44:25 +0200

    updated app

--- src/app.js
+++ src/app.js
@@ -77,7 +77,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "c8ccfe09093c360ab92ce5d129ff41d5";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
