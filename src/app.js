28a8cee4183d1d7c2a8bb8261ef98e8945cc67c3
Katemarkiza <111583089+Katemarkiza@users.noreply.github.com>
2022-09-05 22:10:51 +0200

    console

--- src/app.js
+++ src/app.js
@@ -60,7 +60,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "c8ccfe09093c360ab92ce5d129ff41d5";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
